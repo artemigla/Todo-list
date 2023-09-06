03230fe0a8832dd4522f84aea51985c81c323eaf
artemigla <igla.artem@gmail.com>
2023-09-06 21:22:35 +0300

    Created Todolist.jsx component.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Todolist } from "./components/Todolist";
+
+export const App = () => {
+  return (
+    <div>
+      <Todolist />
+    </div>
+  )
+}
---
+++ src/components/Todolist.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Todolist = () => {
+  return (
+    <div>
+      <input />
+      <button>New task</button>
+    </div>
+  )
+}
