6dff80139e082874c7458027dff39c97d9a46d81
artemigla <igla.artem@gmail.com>
2023-09-22 02:43:51 +0300

    Added redux-persist.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+const persistor = persistStore(store);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import TodoSlice from '../../components/TodoSlice.js'
+import TodoSlice from '../../components/TodoSlice.js';
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from 'redux';
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const reducers = combineReducers({
+  todo: TodoSlice
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = configureStore({
-  reducer: {
-    todo: TodoSlice
-  }
+  reducer: persistedReducer,
+  middleware: [thunk]
 })
